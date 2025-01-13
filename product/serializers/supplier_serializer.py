f9be9182f158a873a33a96ff813dac16ba67e2fc
Rohini-AG <rohiniagklbm2018@gmail.com>
2025-01-13 16:54:13 +0530

    source,unit validation

--- product/serializers/supplier_serializer.py
+++ product/serializers/supplier_serializer.py
@@ -7,19 +7,21 @@ class SuppliersSerializer(serializers.ModelSerializer):
         fields = ['id', 'name','email','address']
 
     def validate_name(self, value):
-        """Validate that the supplier name is unique."""
+        """Validate that the Supplier name is unique."""
         if Suppliers.objects.filter(name=value).exists():
-            raise serializers.ValidationError("A supplier with this name already exists.")
+            raise serializers.ValidationError("A Supplier with this name already exists.")
         return value
         
     def validate_email(self, value):
-        """Validate that the supplier email is unique."""
+        """Validate that the Supplier email is unique."""
         if Suppliers.objects.filter(email=value).exists():
-            raise serializers.ValidationError("A supplier with this email already exists.")
+            raise serializers.ValidationError("A Supplier with this email already exists.")
+        return value
+    
+    def validate_address(self, value):
+        """Validate that the Supplier address is unique."""
+        if Suppliers.objects.filter(address=value).exists():
+            raise serializers.ValidationError("A Supplier with this address already exists.")
         return value
 
-    def validate_address(self, value):
-        """Validate that the supplier address is unique."""
-        if Suppliers.objects.filter(address=value).exists():
-            raise serializers.ValidationError("A supplier with this address already exists.")
-        return value
+    
