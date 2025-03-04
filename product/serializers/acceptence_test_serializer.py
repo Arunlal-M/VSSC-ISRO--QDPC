c72e48ff4045ffa4c276f5fc77d62b60cc66f0b5
Arunlal <arunlal.m2000@gmail.com>
2025-03-04 16:50:45 +0530

    acceptance name validation

--- product/serializers/acceptence_test_serializer.py
+++ product/serializers/acceptence_test_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
+from rest_framework import status
 from qdpc_core_models.models.acceptance_test import AcceptanceTest
-
 
 class AcceptanceTestSerializer(serializers.ModelSerializer):
  
@@ -17,17 +17,35 @@ class AcceptanceTestSerializer(serializers.ModelSerializer):
             'reevaluation_frequency_unit', 'reevaluation_frequency'
         ]
 
+    def validate_name(self, value):
+        if AcceptanceTest.objects.filter(name=value).exists():
+            raise serializers.ValidationError("An acceptance test with this name already exists.")
+        return value
 
     def get_sampling_plan_url(self, obj):
         request = self.context.get('request')
         if request is not None and obj.sampling_plan:
             return request.build_absolute_uri(obj.sampling_plan.url)
         elif obj.sampling_plan:
-            # Manually construct the URL if request is None
             return f"/product{obj.sampling_plan.url}"
         return None
     
-
     def get_unit_name(self, obj):
-        # Access the unit's name field and return it
         return obj.unit.abbreviation if obj.unit else None
+
+    def post(self, request):
+        serializer = AcceptanceTestSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            success = True
+            message = 'Acceptance test added successfully!'
+            data = serializer.data
+            status_code = status.HTTP_201_CREATED
+        else:
+            success = False
+            message = 'Validation failed.'
+            status_code = status.HTTP_400_BAD_REQUEST
+            data = serializer.errors
+
+        return self.render_response(data, success, message, status_code)
